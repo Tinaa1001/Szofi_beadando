5bee831a5023027e43d9ad50a53143073abaac25
Quercoder1 <mateivan418@gmail.com>
2024-12-11 21:18:13 +0100

    kód kész, word kell már csak

---
+++ beadando_LHPSQE/beadando_LHPSQE/EsemenyKezeles.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beadando_LHPSQE
+{
+    public class EsemenyKezeles
+    {
+        // Esemény definíciója
+        public event EventHandler<SzenzorErtekek> SzenzorErtekValtozasEvent;
+
+        // Esemény kiváltása
+        public void OnSzenzorErtekValtozas(SzenzorErtekek ertekek)
+        {
+            SzenzorErtekValtozasEvent?.Invoke(this, ertekek);
+        }
+
+        // Feliratkozás az eseményre
+        public void Feliratkozas()
+        {
+            SzenzorErtekValtozasEvent += (sender, e) =>
+            {
+                // Az esemény kezelése
+                Console.WriteLine($"Esemény: Szenzor {e.SzenzorId} érték: {e.MertErtek} °C, Idő: {e.Ido}");
+            };
+        }
+    }
+}
--- beadando_LHPSQE/beadando_LHPSQE/Program.cs
+++ beadando_LHPSQE/beadando_LHPSQE/Program.cs
@@ -13,6 +13,33 @@ using Newtonsoft.Json; //A JSON nuget package névtere
 
 namespace beadando_LHPSQE
 {
+    // A delegáltat definiáló osztály
+    public class SzenzorAdatfeldolgozo
+    {
+        // Delegált definíciója: Ez egy olyan típus, amely egy metódust reprezentál,
+        // ami két paramétert (string és double) vesz át és nem ad vissza értéket.
+        public delegate void SzenzorAdatKezelo(string szenzorId, double mertErtek);
+
+        // Esemény, amely a delegált típusát használja
+        // Az eseményre más osztályok regisztrálhatnak, hogy végrehajtsák a szükséges műveletet, amikor az esemény kiváltódik.
+        public event SzenzorAdatKezelo AdatErkezett;
+
+        // Adatok feldolgozásának szimulálása
+        // A metódus végigiterál a szenzorok listáján és az adatok generálásával kiváltja az eseményt.
+        public void AdatFeldolgozas(List<Szenzor> szenzorok)
+        {
+            // A foreach ciklus végigmegy az összes szenzoron a listában.
+            foreach (var szenzor in szenzorok)
+            {
+                // Mért érték generálása a szenzor adatgenerálása alapján.
+                double ertek = szenzor.AdatGeneralas();
+
+                // Ha van regisztrált kezelő, akkor az esemény kiváltódik,
+                // és a paraméterként átadott értékek (szenzor ID és mért érték) átadásra kerülnek.
+                AdatErkezett?.Invoke(szenzor.Id, ertek); // Esemény kiváltása, ha van regisztrált kezelő
+            }
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -25,6 +52,14 @@ namespace beadando_LHPSQE
                 new Szenzor("DEF2"),
                 new Szenzor("GHI3")
             };
+
+            //Nagy Máté Iván
+            //Eseménykezelés
+            // Eseménykezelés példányosítása
+            var esemenyKezeles = new EsemenyKezeles();
+
+            // Feliratkozás az eseményre
+            esemenyKezeles.Feliratkozas();
 
             //Tóth Krisztina
             //Adatok adatbázisban való elhelyezése
@@ -46,6 +81,9 @@ namespace beadando_LHPSQE
                         MertErtek = ertek,
                         Ido = DateTime.Now
                     };
+                    
+                     // Esemény kiváltása
+                    esemenyKezeles.OnSzenzorErtekValtozas(adat);
 
                     // Adat mentése a kollekcióba az új változó segítségével egyben
                     collection.Insert(adat);
@@ -79,6 +117,32 @@ namespace beadando_LHPSQE
                 Console.WriteLine("\nA JSON fájl sikeresen elkészült.");    //Visszajelzés a sikeres fileiratásról
 
                 Console.WriteLine("\nA kilépéshez nyomjon le egy billentyűt.");   //Kilépés a futtatásból
+
+                //Nagy Máté
+                //3 LINQ lekérdezés létrehozása
+                var lekerdezesek = new LINQ_lekerdezes("szenzorhalozat.db"); // Létrehozzuk az osztály példányát a megfelelő adatbázis névvel
+
+                Console.WriteLine("Legfrissebb mérések: "); // Legfrissebb mérések lekérdezése
+                var legfrissebbAdatok = lekerdezesek.LegfrissebbAdatok();  // Lekérdezzük a legfrissebb méréseket
+                foreach (var adat in legfrissebbAdatok)
+                {
+                    // Minden szenzor legfrissebb mérésének kiírása
+                    Console.WriteLine($"Szenzor {adat.SzenzorId}: {adat.MertErtek} °C [{adat.Ido}]");
+                }
+
+                Console.WriteLine("\nSzenzorok magas értékekkel (küszöb: 25.0):"); // Szenzorok magas értékekkel való lekérdezése
+                var magasErtekek = lekerdezesek.SzenzorokMagasErtekkel(25.0);  // Lekérdezzük a magas mért értékekkel rendelkező szenzorokat
+                foreach (var szenzorId in magasErtekek)
+                {
+                    // A szenzor azonosítók kiírása, amelyek átléptek a küszöbértéken
+                    Console.WriteLine($"Szenzor ID: {szenzorId}");
+                }
+
+                Console.WriteLine("\nÁtlagos hőmérséklet:"); // Átlagos hőmérséklet lekérdezése
+                var atlagHomerseklet = lekerdezesek.AtlagosHomerseklet();  // Lekérdezzük az átlagos hőmérsékletet
+                Console.WriteLine($"{atlagHomerseklet} °C"); // Az átlagos hőmérséklet kiírása
+
+                
             }
             Console.ReadKey();
         }
